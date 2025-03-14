2cd7601b4975c1860aca5667a0b0c39473d98ee2
Nich <nich.holt04@gmail.com>
2025-03-14 15:09:33 +0000

    idle -> running animations set up

--- Wandering Warlocks/Assets/Scripts/PlayerController.cs
+++ Wandering Warlocks/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Vector2 move;
+    private Animator animator;
 
     public void onMove(InputAction.CallbackContext context)
     {
@@ -13,19 +14,33 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         movePlayer();
+        updateAnimationState();
     }
 
     void movePlayer()
     {
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
+            Vector3 movement = new Vector3(move.x, 0f, move.y);
         
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        
+    }
+
+    void updateAnimationState()
+    {
+        bool isMoving = move.sqrMagnitude > 0;
+
+        Debug.Log("Before: Running = " + animator.GetBool("Running"));
+
+        animator.SetBool("Running", isMoving);
+        animator.SetBool("Idle", !isMoving);
+
+       Debug.Log("After: Running = " + animator.GetBool("Running"));
     }
 }
